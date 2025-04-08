2f30950f3850d50be8ae3a3fbcb6c084b11cf922
Dino Gironi <dino.gironi@bbd.co.za>
2025-04-09 00:14:15 +0200

    Refactor CLI TUI

--- src/GalaxyWiki.CLI/Program.cs
+++ src/GalaxyWiki.CLI/Program.cs
@@ -6,139 +6,47 @@ namespace GalaxyWiki.Cli
     {
         public static void Main(string[] args)
         {
+            //==================== Layout definition ====================//
             Layout layout = new Layout("Root")
                 .SplitColumns(
                     new Layout("Left"),
-                    new Layout("Middle")
-                        .SplitRows(
-                            new Layout("Header"),
-                            new Layout("Terminal")
-                        ),
-                    new Layout("Right")
-                        .SplitRows(
-                            new Layout("Top"),
-                            new Layout("Mid"),
-                            new Layout("Bottom")
-                        )
+                    new Layout("Middle").SplitRows( new Layout("Header"),   new Layout("Terminal")                          ),
+                    new Layout("Right").SplitRows(  new Layout("Top"),      new Layout("Mid"),      new Layout("Bottom")    )
                 );
 
-            // Left panel
-            // Universe Tree
 
+            //==================== Left panel ====================//
+
+            //---------- Construct universe tree ----------//
+            // TODO: Populate via API fetch
             var universe = new Tree("Universe");
 
             var sagA = universe.AddNode("Sagittarius A*");
 
-            var sun = sagA.AddNode("Sun");
-            sun.AddNode("Mercury");
-            sun.AddNode("Venus");
-
-            var earth = sun.AddNode("Earth");
-            earth.AddNode("Moon");
-            earth.AddNode("ISS");
-
-            var mars = sun.AddNode("Mars");
-            mars.AddNode("Phobos");
-            mars.AddNode("Deimos");
-
-            var jupiter = sun.AddNode("Jupiter");
-            jupiter.AddNode("Io");
-            jupiter.AddNode("Europa");
-            jupiter.AddNode("Ganymede");
-            jupiter.AddNode("Callisto");
-            jupiter.AddNode("Amalthea");
-            jupiter.AddNode("Thebe");
-            jupiter.AddNode("Himalia");
-            jupiter.AddNode("Elara");
-            jupiter.AddNode("Lysithea");
-
-            var saturn = sun.AddNode("Saturn");
-            saturn.AddNode("Titan");
-            saturn.AddNode("Enceladus");
-            saturn.AddNode("Iapetus");
-            saturn.AddNode("Rhea");
-            saturn.AddNode("Dione");
-            saturn.AddNode("Tethys");
-            saturn.AddNode("Mimas");
-            saturn.AddNode("Hyperion");
-            saturn.AddNode("Phoebe");
-            saturn.AddNode("Janus");
-            saturn.AddNode("Epimetheus");
-
-            var uranus = sun.AddNode("Uranus");
-            uranus.AddNode("Miranda");
-            uranus.AddNode("Ariel");
-            uranus.AddNode("Umbriel");
-            uranus.AddNode("Titania");
-            uranus.AddNode("Oberon");
-
-            var neptune = sun.AddNode("Neptune");
-            neptune.AddNode("Triton");
-            neptune.AddNode("Nereid");
-            neptune.AddNode("Proteus");
-            neptune.AddNode("Larissa");
-
-            var pluto = sun.AddNode("Pluto");
-            pluto.AddNode("Charon");
-            pluto.AddNode("Hydra");
-            pluto.AddNode("Nix");
-            pluto.AddNode("Kerberos");
-            pluto.AddNode("Styx");            
+            var sun = sagA.AddNode("Sun");                  sun.AddNodes("Mercury", "Venus");
+            var earth = sun.AddNode("Earth");               earth.AddNodes("Moon", "ISS");
+            var mars = sun.AddNode("Mars");                 mars.AddNodes("Phobos", "Deimos");
+            var jupiter = sun.AddNode("Jupiter");           jupiter.AddNodes("Io", "Europa", "Ganymede", "Callisto", "Amalthea", "Thebe", "Himalia", "Elara", "Lysithea");
+            var saturn = sun.AddNode("Saturn");             saturn.AddNodes("Titan", "Enceladus", "Iapetus", "Rhea", "Dione", "Tethys", "Mimas", "Hyperion", "Phoebe", "Janus", "Epimetheus");
+            var uranus = sun.AddNode("Uranus");             uranus.AddNodes("Miranda", "Ariel", "Umbriel", "Titania", "Oberon");
+            var neptune = sun.AddNode("Neptune");           neptune.AddNodes("Triton", "Nereid", "Proteus", "Larissa");
+            var pluto = sun.AddNode("Pluto");               pluto.AddNodes("Charon", "Hydra", "Nix", "Kerberos", "Styx");            
 
             layout["Left"].Update(
-                new Panel(
-                    universe
-                )
+                new Panel(universe)
                 .RoundedBorder()
                 .Header("[cyan] Universe Tree :deciduous_tree: [/]")
                 .Expand()
             );
 
-            CanvasImage image = new CanvasImage("../../assets/earth.png").MaxWidth(12);
-            
-            layout["Right"]["Mid"].Update(
-                new Panel(
-                    Align.Center(
-                        image,
-                        VerticalAlignment.Middle
-                    )
-                )
-                .RoundedBorder()
-                .Header("[cyan] Image :camera: [/]")
-                .Expand()
-            );
 
-            var command = AnsiConsole.Prompt(
-                new SelectionPrompt<string>()
-                    .Title("Choose your command:")
-                    .PageSize(10)
-                    .MoreChoicesText("[grey](Move up and down to reveal more options)[/]")
-                    .AddChoices([
-                        "info",
-                        "comment",
-                        "cd"
-                    ])
-            );
+            //==================== Middle panel ====================//
 
-            layout["Middle"]["Terminal"].Update(
-                new Panel(
-                    command
-                )
-                .RoundedBorder()
-                .Header("[cyan] Terminal [/]")
-                .Expand()
-            )
-            .Ratio(2);
-
-            layout["Middle"].Ratio(2);
-
+            //---------- Add banner ----------//
             layout["Middle"]["Header"].Update(
                 new Panel(
                     Align.Left(
-                        new FigletText(
-                            FigletFont.Load("../../assets/starwars.flf"),
-                            "Galaxy Wiki"
-                        )
+                        new FigletText(FigletFont.Load("../../assets/starwars.flf"), "Galaxy Wiki")
                         .Centered()
                         .Color(Color.Aqua),
                         VerticalAlignment.Bottom
@@ -148,18 +56,50 @@ namespace GalaxyWiki.Cli
                 .Expand()
             );
 
+            //---------- Add prompt ----------//
+            var command = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .Title("Choose your command:")
+                    .PageSize(10)
+                    .MoreChoicesText("[grey](Move up and down to reveal more options)[/]")
+                    .AddChoices([ "info", "comment", "cd" ])
+            );
+
+            layout["Middle"]["Terminal"].Update(
+                new Panel(command)
+                .RoundedBorder()
+                .Header("[cyan] Terminal [/]")
+                .Expand()
+            )
+            .Ratio(2);
+
+            layout["Middle"].Ratio(2);
+
+
+            //==================== Right panel ====================//
+
+            //---------- Add image ----------//
+            CanvasImage image = new CanvasImage("../../assets/earth.png").MaxWidth(12);
+            
+            layout["Right"]["Mid"].Update(
+                new Panel(Align.Center(image, VerticalAlignment.Middle))
+                .RoundedBorder()
+                .Header("[cyan] Image :camera: [/]")
+                .Expand()
+            );
+
+
+            //---------- Add chatbot ----------//
             layout["Right"]["Bottom"].Update(
                 new Panel(
-                    Align.Center(
-                        new Markup("This is where the chatbot will be"),
-                        VerticalAlignment.Middle
-                    )
+                    Align.Center(new Markup("This is where the chatbot will be"), VerticalAlignment.Middle)
                 )
                 .RoundedBorder()
                 .Header("[cyan] Galaxy Bot :robot::sparkles: [/]")
                 .Expand()
             );
 
+            //---------- Add useless calendar ----------//
             layout["Right"]["Top"].Update(
                 new Panel(
                     Align.Center(
@@ -175,6 +115,9 @@ namespace GalaxyWiki.Cli
                 .Header("[cyan] Calendar :calendar: [/]")
                 .Expand()
             );
+
+
+            //==================== Output ====================//
 
             AnsiConsole.Write(layout);
         }
