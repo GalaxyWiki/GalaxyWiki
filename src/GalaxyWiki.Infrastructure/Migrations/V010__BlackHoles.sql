4a152aaf51e2e1693e821f5c80753fa477f4463f
Tevlen-Naidoo-BBD <tevlen.naidoo@bbd.co.za>
2025-04-12 09:13:28 +0200

    Added the universe an updated the orbit id's

--- src/GalaxyWiki.Infrastructure/Migrations/V010__BlackHoles.sql
+++ src/GalaxyWiki.Infrastructure/Migrations/V010__BlackHoles.sql
@@ -1,6 +1,6 @@
 -- Black holes in other galaxies
 INSERT INTO celestial_bodies (body_name, orbits, body_type_id) VALUES
-('M87*', 17, 6), -- Black hole at center of M87, first imaged black hole
-('TON 618 Black Hole', 63, 6), -- One of the most massive known black holes
-('OJ 287 Binary Black Hole', 55, 6),
-('Andromeda Black Hole', 2, 6); -- Supermassive black hole at center of Andromeda
+('M87*', 18, 6), -- Black hole at center of M87, first imaged black hole
+('TON 618 Black Hole', 64, 6), -- One of the most massive known black holes
+('OJ 287 Binary Black Hole', 56, 6),
+('Andromeda Black Hole', 3, 6); -- Supermassive black hole at center of Andromeda
